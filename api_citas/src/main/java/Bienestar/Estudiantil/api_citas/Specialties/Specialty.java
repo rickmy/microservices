c44a171c358bbc6289d388c5040deddabbe77e74
alexander larco <papamonthalvo@hotmail.com>
2022-12-27 16:34:01 -0500

    CRUD DE ESPECIALIDADES

---
+++ api_citas/src/main/java/Bienestar/Estudiantil/api_citas/Specialties/Specialty.java
@@ -0,0 +1,21 @@
+package Bienestar.Estudiantil.api_citas.Specialties;
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Specialty {
+
+ @Id
+ @GeneratedValue(strategy = GenerationType.AUTO)
+
+ private Long id;
+ private String especialty_name;
+ private Boolean condition;
+ 
+}
