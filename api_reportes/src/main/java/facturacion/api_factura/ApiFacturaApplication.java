543e992708c631ac41f1c54ef963a43cb9531591
JoshuaR8787 <120408141+JoshuaR8787@users.noreply.github.com>
2023-01-24 23:37:48 -0500

    Add files via upload

---
+++ api_reportes/src/main/java/facturacion/api_factura/ApiFacturaApplication.java
@@ -0,0 +1,13 @@
+package facturacion.api_factura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFacturaApplication.class, args);
+	}
+
+}
