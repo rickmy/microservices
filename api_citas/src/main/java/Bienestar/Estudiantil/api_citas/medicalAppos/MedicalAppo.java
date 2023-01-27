dec32090008fa5908ae8de7123bccd322236805d
Joshua <fiedrojas87@gmail.com>
2023-01-27 10:25:34 -0500

    arrays catalogos

--- api_citas/src/main/java/Bienestar/Estudiantil/api_citas/medicalAppos/MedicalAppo.java
+++ api_citas/src/main/java/Bienestar/Estudiantil/api_citas/medicalAppos/MedicalAppo.java
@@ -1,10 +1,14 @@
 package Bienestar.Estudiantil.api_citas.medicalAppos;
 import java.sql.Date;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Entity
@@ -14,13 +18,25 @@ public class MedicalAppo {
  @Id
  @GeneratedValue(strategy = GenerationType.AUTO)
 
- private Long id;
- private Date attentionDate;
- private String serviceTime;
- private String reasonForAppointment;
- private String medicalObservations;
- private Boolean condition;
- private Integer patientId;
- private Integer doctorId;
- 
-}
+    private Long id;
+    private Date attentionDate;
+    private String serviceTime;
+    private String reasonForAppointment;
+    private String medicalObservations;
+    private Boolean condition;
+    private Integer patientId;
+    private Integer doctorId;
+
+   // @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+   // @JoinColumn(name = "medical_appo_id")
+    //private List<Treatment> treatments = new ArrayList<>();
+
+   // @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+   // @JoinColumn(name = "medical_appo_id")
+   // private List<Symptom> symptoms = new ArrayList<>();
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "medical_appo_id")
+    private List<Diagnostic> diagnostics = new ArrayList<>();
+
+}   
