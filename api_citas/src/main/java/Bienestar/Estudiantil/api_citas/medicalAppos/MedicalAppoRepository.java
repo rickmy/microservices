d6120e8fec6885aaacfeed931f7758f15c574511
Emerson Alexander <eam.larco@yavirac.edu.ec>
2023-02-02 00:02:53 -0500

    arrego de peticiones

--- api_citas/src/main/java/Bienestar/Estudiantil/api_citas/medicalAppos/MedicalAppoRepository.java
+++ api_citas/src/main/java/Bienestar/Estudiantil/api_citas/medicalAppos/MedicalAppoRepository.java
@@ -2,10 +2,11 @@ package Bienestar.Estudiantil.api_citas.medicalAppos;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MedicalAppoRepository extends JpaRepository<MedicalAppo, Long> {
+import org.springframework.data.repository.CrudRepository;
+
+public interface MedicalAppoRepository extends CrudRepository<MedicalAppo, Long> {
 
  public List<MedicalAppo> findAll();
-
+public List<MedicalAppo> findByCondition(Boolean condition);
 }
